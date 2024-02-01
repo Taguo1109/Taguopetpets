c6d67d6e2c8befccd1324e347ed3b371bc4cbb93
Taguo1109 <142570797+Taguo1109@users.noreply.github.com>
2024-02-01 23:59:59 +0800

    delete_kk

--- kk/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  devServer:{
-    port: 1109,//使用不同端口
-  }
-}
