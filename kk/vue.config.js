3abf36ff8066b369a2397dd38c83135d30228fab
Taguo1109 <142570797+Taguo1109@users.noreply.github.com>
2024-02-01 23:46:22 +0800

    endpoint_serve

--- kk/vue.config.js
+++ kk/vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer:{
+    port: 1109,//使用不同端口
+  }
+}
